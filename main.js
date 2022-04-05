1c39e9d7dc72b1f2f90a29236ef36c82326a540c
mrcod3r.ir@gmail.com <hiwhoru@yahoo.com>
2022-04-05 18:40:59 +0430

    add foundation and add todolist 50%

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 Vue.createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        doList:[
+          {
+            title:"Lorem, ipsum.",
+            completed:false,
+          },
+          {
+            title:"Lorem, ipsum dolor.",
+            completed:false,
+          },
+          {
+            title:"asjdfasdf ",
+            completed:true,
+          },
+          {
+            title:"as4df6 Lorem, ipsum.",
+            completed:false,
+          },
+        ]
       }
     }
   }).mount('#app')
