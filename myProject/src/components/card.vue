5d05bf77115521017427f8a5c29d8734c7776608
raadschooljs <raadschooljs@gmail.com>
2022-04-24 19:08:49 +0430

    add card.vue

---
+++ myProject/src/components/card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+        <img src="../../public/pic.png" alt="card" >
+        <h2>this is title of card</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, distinctio harum tempore corporis asperiores magnam suscipit. Sequi sit porro optio neque a. Alias, laborum eum rem eius enim repudiandae saepe?</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.card {
+  width: 300px;
+  height: auto;
+  padding: 1rem;
+  border: solid;
+  border-radius: 20px;
+  margin:20px auto;
+}
+.card img {
+  width: 100%;
+}
+</style>
