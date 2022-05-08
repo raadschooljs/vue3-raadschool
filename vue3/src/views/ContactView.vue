ba8019b53f4b8c41ea31b474003cee17d5652447
raadschooljs <raadschooljs@gmail.com>
2022-05-08 21:08:39 +0430

    add contact page

--- vue3/src/views/ContactView.vue
+++ vue3/src/views/ContactView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="contact">
+    <h1>This is an contact page</h1>
   </div>
 </template>
 
-<style scoped>
+<style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
-h1 {
-  color:red;
 }
 </style>
